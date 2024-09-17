64b8b7133eb087880b87957ed737f9dbaf4c4bee
Panagis Katsivelis <panagis.katsivelis@venthic.com>
2024-09-17 21:50:28 +0300

    new branch conf

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `WaterPath Web`,
-    siteUrl: `https://waterpath-project.github.io`
+    siteUrl: `https://waterpath-project.github.io/`
   },
   plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
